9c6cb57329f6b0ba9439d509fb71911d79cf087e
unknown <jva.krishna@gmail.com>
2022-10-08 21:52:06 +0530

    shell project commit

---
+++ src/main/java/com/bk/FamilyBudjetPlan/FamBpController.java
@@ -0,0 +1,16 @@
+package com.bk.FamilyBudjetPlan;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.desktop.SystemSleepEvent;
+@RestController
+
+public class FamBpController {
+    @RequestMapping("/FBP")
+    public String Fambp()
+    {
+        return "Family budjet begins here ";
+    }
+
+}
---
+++ src/main/java/com/bk/FamilyBudjetPlan/FamilyBudjetPlanApplication.java
@@ -0,0 +1,13 @@
+package com.bk.FamilyBudjetPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudjetPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBudjetPlanApplication.class, args);
+	}
+
+}
