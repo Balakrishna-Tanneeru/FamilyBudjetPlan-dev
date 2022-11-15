5c5c37b161dc5320a6e3b63231808460de262ae6
Balakrishna-Tanneeru <jva.krishna@gmail.com>
2022-11-15 07:35:34 +0530

    Added //

--- src/main/java/com/bk/FamilyBudjetPlan/FamBpController.java
+++ src/main/java/com/bk/FamilyBudjetPlan/FamBpController.java
@@ -1,9 +1,16 @@
 package com.bk.FamilyBudjetPlan;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.desktop.SystemSleepEvent;
+import java.rmi.ServerException;
+
 @RestController
 
 public class FamBpController {
@@ -12,5 +19,16 @@ public class FamBpController {
     {
         return "Family budjet begins here us123 ";
     }
+
+    @PostMapping(path = "/Groc",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Groc> create(@RequestBody Groc newGroc) {
+        Groc grc = grocService.save(newGroc);
+        if (grc == null) {
+            throw new ServerException();
+        } else {
+            return new ResponseEntity<>(grc, HttpStatus.CREATED);
+        }
 
 }
